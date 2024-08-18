8fd3756c14c1745d87773334eaa7a4775147d6d0
pafullb007 <prafull007@gmail.com>
2024-08-18 18:02:17 +0530

    newbranch1

--- tearrformclusteraws/variables.tf
+++ tearrformclusteraws/variables.tf
@@ -1,7 +1,7 @@
 variable "region" {
   description = "The AWS region to deploy the cluster"
   type        = string
-  default     = "us-west-2"
+  default     = "us-east-1"
 }
 
 variable "cluster_name" {
@@ -25,9 +25,4 @@ variable "access_key" {
 }
 variable "secret_key" {
     default = ""
-}
-
-
-variable "region" {
-    default = "us-east-1"
 }
