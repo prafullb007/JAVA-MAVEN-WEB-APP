c3b4aab5ed08825ed0b613850d181d0a3cf3d725
pafullb007 <prafull007@gmail.com>
2024-08-31 14:48:22 +0530

    test1

--- tearrformclusteraws/module_eks/main.tf
+++ tearrformclusteraws/module_eks/main.tf
@@ -53,10 +53,6 @@ resource "aws_eks_node_group" "eks_node_group" {
 
   instance_types = [var.instance_type]
 
-  remote_access {
-    ec2_ssh_key = var.key_name
-  }
-
   tags = var.tags
 }
 
@@ -90,44 +86,4 @@ resource "aws_iam_role_policy_attachment" "eks_node_AmazonEC2ContainerRegistryRe
 resource "aws_iam_role_policy_attachment" "eks_node_AmazonEKS_CNI_Policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
   role       = aws_iam_role.eks_node_role.name
-}
-
-resource "aws_eks_cluster" "eks" {
-  name     = var.cluster_name
-  role_arn = aws_iam_role.eks_cluster_role.arn
-
-  vpc_config {
-    subnet_ids = var.subnet_ids
-  }
-
-  version = var.cluster_version
-
-  tags = var.tags
-}
-
-resource "aws_iam_role" "eks_cluster_role" {
-  name = "${var.cluster_name}-eks-cluster-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "eks.amazonaws.com"
-        }
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "eks_cluster_AmazonEKSClusterPolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = aws_iam_role.eks_cluster_role.name
-}
-
-resource "aws_iam_role_policy_attachment" "eks_cluster_AmazonEKSVPCResourceController" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
-  role       = aws_iam_role.eks_cluster_role.name
 }
