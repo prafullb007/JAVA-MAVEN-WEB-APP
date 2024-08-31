9eb2ea6b624b52d0afc17250f8fc78419fefa67a
pafullb007 <prafull007@gmail.com>
2024-08-31 15:01:25 +0530

    test1

--- tearrformclusteraws/module_eks/main.tf
+++ tearrformclusteraws/module_eks/main.tf
@@ -43,7 +43,7 @@ resource "aws_eks_node_group" "eks_node_group" {
   cluster_name    = aws_eks_cluster.eks.name
   node_group_name = "${var.cluster_name}-node-group"
   node_role_arn   = aws_iam_role.eks_node_role.arn
-  subnets         = var.subnet_ids
+  subnet_ids         = var.subnet_ids
 
   scaling_config {
     desired_size = var.desired_capacity
