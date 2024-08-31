ee97fe0c921a69784fb01901164ab62dc32ce0ac
pafullb007 <prafull007@gmail.com>
2024-08-31 13:39:36 +0530

    test1

---
+++ tearrformclusteraws/module_eks/main.tf
@@ -0,0 +1,29 @@
+
+module "eks_module" {
+  source          = "terraform-aws-modules/eks/aws"
+  cluster_name    = "my-cluster"
+  cluster_version = "1.24"  # Update to your desired Kubernetes version
+  vpc_subnets     = ["subnet-03f3cd99a48a2db7c", "subnet-001a3b80bd44fbbd4"]  # Update to your subnets
+  vpc_id          = "vpc-0532cc35649634df9"  # Update to your VPC ID
+
+  node_groups = {
+    eks_nodes = {
+      desired_capacity = 2
+      max_capacity     = 3
+      min_capacity     = 1
+
+      instance_type = "t3.medium"
+
+      key_name = "your-keypair"  # Update to your keypair name
+
+      additional_tags = {
+        Name = "eks-node"
+      }
+    }
+  }
+
+  tags = {
+    Environment = "dev"
+    Project     = "eks-terraform"
+  }
+}
---
+++ tearrformclusteraws/module_eks/variables.tf
@@ -0,0 +1,29 @@
+variable "region" {
+  description = "The AWS region to deploy the cluster"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "cluster_name" {
+  description = "The name of the EKS cluster"
+  type        = string
+  default     = "my-cluster"
+}
+/*
+variable "vpc_id" {
+  description = "VPC ID where the EKS cluster will be deployed"
+  type        = string
+}
+
+variable "subnets" {
+  description = "Subnets where the EKS cluster and nodes will be deployed"
+  type        = list(string)
+}
+
+variable "access_key" {
+    default = ""
+}
+variable "secret_key" {
+    default = ""
+}
+*/
